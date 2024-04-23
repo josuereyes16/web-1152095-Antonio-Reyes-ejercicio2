9f89d19035ecdcb0ebd8399037d938aa158168a3
JOSUE REYES <antoniojosuerp@ufps.edu.co>
2024-04-23 12:32:49 -0500

    se incluye la vista tasks funcional

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.http import JsonResponse
+from django.core.serializers import serialize
+
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all()
+    data = serialize("python", tasks)
+    return JsonResponse(data, safe=False)
